87dcbd718d830ad4151eb27fead90d20c8df84a0
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-12-22 11:51:30 +0100

    chore: setting up base colors

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,18 +1,25 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				white: {
+					primary: "#FBFBEF",
+				},
+				black: {
+					primary: "#2A2A2A",
+				},
+				orange: {
+					primary: "#FF673D",
+				},
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
