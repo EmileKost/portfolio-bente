a80873e19aa7174fa5bb4c586124b70cea7b2ba4
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2025-01-05 11:04:13 +0100

    fix: styling

--- app/src/app/(app)/_components/ProjectCard.tsx
+++ app/src/app/(app)/_components/ProjectCard.tsx
@@ -12,49 +12,55 @@ import { twMerge } from "tailwind-merge";
 import type { ProjectCard as ProjectCardType } from "@/types/project";
 
 export const ProjectCard = forwardRef(function ProjectCard(
-	{ title, url, image, video, className, projectType, index }: ProjectCardType,
+	{
+		title,
+		url,
+		image,
+		video,
+		className,
+		projectType,
+		index,
+		length,
+	}: ProjectCardType,
 	ref: Ref<HTMLAnchorElement> | undefined
 ) {
 	const [position, setPosition] = useState({ x: 0, y: 0 });
 	const [isHovering, setIsHovering] = useState<boolean>(false);
 	const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
-	const containerRef = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLAnchorElement>(null);
 
 	return (
 		<Link
-			ref={ref}
 			href={`/projects/${url}`}
-			className={twMerge("aspect-[3/4] relative cursor-none", className)}>
-			<div
-				ref={containerRef}
-				onMouseMove={(e) => {
-					handleMousePosition(e, containerRef, setPosition);
-					setIsHovering(true);
-				}}
-				onMouseLeave={() => setIsHovering(false)}>
-				<Media
-					image={image}
-					video={video}
-					title={title}
-					isPlaying={isPlaying}
-				/>
-				<div className="z-20 absolute bottom-0 left-0 w-full px-2 pb-1 flex justify-between text-base text-white-primary">
-					<span>
-						{index + 1}/{length}
-					</span>
-					<div className="flex gap-3 uppercase !text-white-primary">
-						<h3>{title}</h3>
-						<span className="opacity-80">{projectType}</span>
-					</div>
+			ref={containerRef}
+			className={twMerge("aspect-[3/4] relative cursor-none", className)}
+			onMouseMove={(e) => {
+				handleMousePosition(e, containerRef, setPosition);
+				setIsHovering(true);
+			}}
+			onMouseLeave={() => setIsHovering(false)}>
+			<Media
+				image={image}
+				video={video}
+				title={title}
+				isPlaying={isPlaying}
+			/>
+			<div className="z-20 absolute bottom-0 left-0 w-full px-2 pb-1 flex justify-between text-base text-white-primary">
+				<span>
+					{index + 1}/{length}
+				</span>
+				<div className="flex gap-3 uppercase !text-white-primary">
+					<h3>{title}</h3>
+					<span className="opacity-80">{projectType}</span>
 				</div>
-				<Cursor
-					isHovering={isHovering}
-					className="bg-black-dark text-white-primary"
-					content="View"
-					position={position}
-				/>
 			</div>
+			<Cursor
+				isHovering={isHovering}
+				className="bg-black-dark text-white-primary"
+				content="View"
+				position={position}
+			/>
 		</Link>
 	);
 });
