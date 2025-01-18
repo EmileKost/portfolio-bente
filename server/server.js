41f4262966ee87f05d4f96d41d8671f2e1584f33
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2025-01-18 14:31:17 +0100

    feat: connect to db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+mongoose.connect(process.env.DB_URI);
 
 app.listen(process.env.PORT, () => {
   console.log("Port listening: ", process.env.PORT);
