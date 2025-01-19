b9149ec144db21130a71cce829521cd1a5ec5e97
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2025-01-19 13:36:18 +0100

    feat: setting up about schema's

---
+++ server/schemas/about.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Media = require("./media");
+const Skills = require("./skills");
+const Experience = require("./experience");
+
+const aboutSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  randomFacts: { type: [String], minLength: 1, maxLenght: 3, required: true },
+  heroImg: Media,
+  description: { type: String, required: true },
+  skills: [Skills],
+  experience: {
+    type: [Experience],
+    maxLength: 12,
+  },
+});
+
+const About = mongoose.model("About", aboutSchema);
+
+module.exports = About;
---
+++ server/schemas/experience.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const experienceSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  date: { type: Date, required: true },
+  description: { type: String, required: true },
+});
+
+const Experience = mongoose.model("Experience", experienceSchema);
+
+module.exports = Experience;
