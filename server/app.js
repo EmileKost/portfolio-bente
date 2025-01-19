35f7f6540006ef9015da6b7c1561f1d2ba6f33a1
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2025-01-19 13:47:12 +0100

    feat: settig up mrogan for logging

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const userRouter = require("./routes/userRouter");
 const projectsRouter = require("./routes/projectsRouter");
+const aboutRouter = require("./routes/aboutRouter");
 
 const app = express();
 
 // middleware
+if (process.env.ENV === "development") {
+  app.use(morgan("tiny"));
+}
+
 app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/projects", projectsRouter);
+app.use("/about", aboutRouter);
 
 module.exports = app;
