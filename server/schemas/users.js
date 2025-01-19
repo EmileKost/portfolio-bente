69a224fc94127c7f4a88200fbda86048155950f4
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2025-01-19 13:54:41 +0100

    feat: adding roles to user

--- server/schemas/users.js
+++ server/schemas/users.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: String,
   username: String,
+  roles: {
+    type: [String],
+    enum: ["admin", "root"],
+    required: true,
+  },
   password: {
     type: String,
     required: true,
