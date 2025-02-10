f867e01fd411ff23eb251cd5512b36f467177031
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2025-02-10 19:00:38 +0100

    fix: remove boilerplate

--- app/src/components/HomeSlider.tsx
+++ app/src/components/HomeSlider.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { forwardRef, useRef, useEffect } from "react";
-import type { Ref } from "react";
+import { useRef, useEffect } from "react";
 
 import type { Media as MediaType } from "@/types/types.common";
+import Image from "next/image";
 import Link from "next/link";
-import { PropsWithChildren } from "react";
 
-import { Cursor } from "./Cursor";
-import { Media } from "./Media";
 import { useMouseMovement } from "@/hooks/useMouseMovement";
 
 import { gsap } from "gsap";
@@ -30,79 +27,25 @@ type HomeSliderProps = {
 };
 
 export const HomeSlider = ({ projects }: HomeSliderProps) => {
-	const sliderContainerRef = useRef<HTMLDivElement>(null);
-	const sliderRailsRef = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		function amountToScroll(): number {
-			if (sliderRailsRef.current) {
-				return -sliderRailsRef.current.offsetWidth - window.innerWidth;
-			}
-
-			return 0;
-		}
-
-		if (sliderContainerRef.current && sliderRailsRef.current) {
-			const sliderAnimation = gsap.to(sliderRailsRef.current, {
-				x: amountToScroll,
-			});
-
-			// TODO:
-			// - Fix smoother end
-			// - Why not ending on width end?
-
-			ScrollTrigger.create({
-				trigger: sliderContainerRef.current,
-				pin: true,
-				start: "top 0%",
-				end: "+=" + amountToScroll() * -1,
-				scrub: 1.75,
-				markers: true,
-				pinSpacing: true,
-				invalidateOnRefresh: true,
-				animation: sliderAnimation,
-			});
-		}
-
-		return;
-	});
-
 	return (
 		<>
-			<div
-				ref={sliderContainerRef}
-				id="test"
-				className="w-full md:h-screen overflow-hidden flex flex-col justify-center items-center">
-				<div className="w-full h-full py-2 md:py-9 flex justify-center items-center flex-row overflow-hidden flex-shrink-0">
-					<SliderRails ref={sliderRailsRef}>
-						{projects.length > 0 &&
-							projects.map((project, idx) => (
-								<SliderCard
-									key={project.title}
-									project={project}
-									index={idx}
-									length={projects.length}
-								/>
-							))}
-					</SliderRails>
-				</div>
+			<div className="relative w-full h-screen flex justify-center items-center overflow-hidden">
+				{projects.length > 0 && (
+					<div className="w-full flex gap-1">
+						{projects.map((project, idx) => (
+							<SliderCard
+								key={project.title}
+								project={project}
+								length={projects.length}
+								index={idx}
+							/>
+						))}
+					</div>
+				)}
 			</div>
 		</>
 	);
 };
-
-export const SliderRails = forwardRef(function SliderRails(
-	{ children }: PropsWithChildren,
-	ref: Ref<HTMLDivElement>
-) {
-	return (
-		<div
-			ref={ref}
-			className="w-auto h-full flex flex-col gap-1 md:flex-row md:gap-2 bg-black-primary">
-			<>{children}</>
-		</div>
-	);
-});
 
 const SliderCard = ({
 	project,
@@ -115,44 +58,35 @@ const SliderCard = ({
 }) => {
 	const cursorRef = useRef(null);
 
+	// Add cursor
 	const { isHovering, position } = useMouseMovement(cursorRef);
 
 	return (
 		<Link
 			ref={cursorRef}
 			href={`/projects/${project.slug}`}
-			className="slider-item relative w-full md:w-auto h-auto md:max-h-[80vh] aspect-[3/4] flex justify-center items-center shrink-0 overflow-hidden bg-black-primary">
-			<Media
-				image={{
-					src: project.image.src,
-					alt: "image",
-				}}
-				video={project.video}
-				className="aspect-[3/4]"
-				cursor={
-					<Cursor
-						isVisible={isHovering}
-						position={position}
-						text={project.cursor ?? "See project"}
-						className={{
-							text: "text-white-primary text-xxs font-light",
-							container: "bg-red-500 bg-opacity-80 mix-blend-difference",
-						}}
-					/>
-				}
-			/>
-			<div className="absolute w-full bottom-0 left-0 p-2 md:p-4 flex justify-between">
-				<div>
-					<h5 className="text-white-primary text-xxs md:text-base">{`${
-						index + 1
-					}/${length}`}</h5>
-				</div>
-				<div className="flex space-x-3 text-xxs md:text-base">
-					<h4 className="text-white-primary">{project.title}</h4>
-					<h4 className="hidden md:inline-block font-light text-white-primary">
+			className="w-full aspect-[3/4] md:max-w-[550px] h-auto relative shrink-0 flex items-end overflow-hidden">
+			<div className="w-full flex justify-between z-30 px-2 pb-3 bg-gradient-to-t from-black-primary/80 to-transparent">
+				<h4 className="text-xxs md:text-base text-white-primary">{`${
+					index + 1
+				}/${length}`}</h4>
+				<div className="flex gap-2">
+					<h2 className="text-xxs md:text-base flex text-white-primary">
+						{project.title}
+					</h2>
+					<h4 className="text-xxs md:text-base flex text-white-primary">
 						{project.profession}
 					</h4>
 				</div>
+			</div>
+			<div className="w-full h-full absolute top-0 left-0 flex justify-center items-center overflow-hidden">
+				<Image
+					width={520}
+					height={680}
+					alt={project.image.alt}
+					src={project.image.src}
+					className="w-full h-full object-cover"
+				/>
 			</div>
 		</Link>
 	);
