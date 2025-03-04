ee5f83c02dda06ebb04eb5fe132f977b00630abf
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2025-03-04 21:20:41 +0100

    feat: handle using svg in component form

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
 			rules: {
 				"*.svg": {
 					loaders: ["@svgr/webpack"],
-					as: "*.js",
+					as: "*.ts",
 				},
 			},
 		},
--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Logo from "@/assets/logo-large.svg";
 
 export const TheHeader = () => {
 	return (
 		<header className="fixed top-0 left-0 w-full py-8 px-4 flex justify-between items-center">
+			<Logo />
 			<Link
 				href={"/"}
 				className="uppercase text-black-primary text-base md:text-lg">
